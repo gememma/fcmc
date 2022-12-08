c67731cae3cb326b8b67083fe4380196784b5e69
Gem Tipper <gemtipper@gmail.com>
2022-12-08 13:14:51 +0000

    Add LambdaTerm type and method to get Church numerals

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+#![feature(box_patterns, box_syntax)]
+
+use crate::lamdaterm::LambdaTerm;
+use crate::LambdaTerm::{Apply, Lambda};
+
+pub mod lamdaterm;
+
+fn main() {
+    let example = box LambdaTerm::new_var("a");
+    let example2 = box Lambda {
+        arg: "x".to_string(),
+        body: example,
+    };
+    let example3 = box Lambda {
+        arg: "y".to_string(),
+        body: Box::new(LambdaTerm::new_var("y")),
+    };
+    let example4 = box Apply {
+        t1: example2,
+        t2: example3,
+    };
+    println!("{}", example4);
+
+    let example5 = box Lambda {
+        arg: "a".to_string(),
+        body: box Lambda {
+            arg: "x".to_string(),
+            body: box Apply {
+                t1: box Apply {
+                    t1: box Lambda {
+                        arg: "y".to_string(),
+                        body: box LambdaTerm::new_var("a"),
+                    },
+                    t2: box LambdaTerm::new_var("x"),
+                },
+                t2: box LambdaTerm::new_var("b"),
+            },
+        },
+    };
+    println!("{}", example5);
+
+    println!("{}", LambdaTerm::new_num(5));
+}
